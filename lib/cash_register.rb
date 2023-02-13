4af36707e2669e992e5b9e8ab4e2bfff25343756
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-02-13 11:45:40 +0000

    complete lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
-
+class CashRegister
+    attr_accessor :total, :discount
+  
+    def initialize(discount = nil)
+      @total = 0
+      @discount = discount
+      @items = []
+    end
+  
+    def add_item(title, price, quantity = 1)
+      @total += price * quantity
+      quantity.times { @items << {title: title, value: (price * quantity)} }
+    end
+  
+    def apply_discount
+      if @discount.nil?
+        return "There is no discount to apply."
+      else
+        @total *= (1 - (@discount.to_f / 100))
+        return "After the discount, the total comes to $#{@total.to_i}."
+      end
+    end
+  
+    def items
+        @items.map{|item| item[:title]}
+    end
+  
+    def void_last_transaction
+      return @total if @items.empty?
+      item_price = @items.pop
+      @total -= item_price[:value]
+    end
+  end
